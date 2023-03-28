9dd2db43d1d9baeec86ea903e4acac7fc80c30fa
Albja <alban.jaunet@sncf.fr>
2023-03-28 14:13:45 +0200

    done with the bonus

--- myFirstApp/src/app/user-profile/user-profile.component.ts
+++ myFirstApp/src/app/user-profile/user-profile.component.ts
@@ -3,23 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
-  user = {
-    name : 'J',
-    firstName : 'Alban',
-    age : 33,
-    quote :"",
-    photo : "https://randomuser.me/api/portraits/lego/6.jpg",
- 
-
-  
-  };
-  displayUserAge= false;
-  DisplayAge() {
-    this.displayUserAge = !this.displayUserAge }
-
+  user = new User(
+    'Ja*',
+    'Alban',
+    33,
+    '',
+    'https://randomuser.me/api/portraits/lego/5.jpg'
+  );
 }
 
+class User {
+  name: string;
+  firstName: string;
+  age: number;
+  quote: string;
+  photo: string;
+  displayUserAge = false;
 
+  constructor(
+    name: string,
+    firstName: string,
+    age: number,
+    quote: '',
+    photo: string
+  ) {
+    this.name = name;
+    this.firstName = firstName;
+    this.age = age;
+    this.quote = quote;
+    this.photo = photo;
+  }
+
+  displayAge(): void {
+    this.displayUserAge = !this.displayUserAge;
+  }
+}
