55d49d0e1cc80ff625596b77a55c388958ceb5eb
Albja <alban.jaunet@sncf.fr>
2023-03-30 10:31:34 +0200

    correcting defaut path

--- myFirstApp/src/app/app.module.ts
+++ myFirstApp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule } from '@angular/router'
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
